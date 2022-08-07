dbf3745a4e91d4fcbf48c766a8d6747f6bd4efd6
frantz3 <98121677+frantz3@users.noreply.github.com>
2022-08-07 17:16:50 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("DOMContentLoaded", () => {
+ const form = document.querySelector('#create-task-from')
+ const newTaskPriority = document.getElementById("new-task-priority")
+   
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const taskDesc = event.target["new-task-description"]
+
+      const ul = document.querySelector("#tasks")
+      
+
+      li.textContent = taskDesc
+
+      form.addEventListener('submit', createNewTask)
+
+      const createNewTask = e => {
+        e.preventDefault
+        const li = document.createElement('li');
+        li.innerText = taskDesc.value;
+
+        appendLi(li)
+        event.target.reset()
+
+        const appendLi = task =>{
+          document.getElementById(('tasks').appendChild(task))
+        }
+
+      }
+
+      const button = document.createElement("button")
+      button.textContent = "x"
+      button.addEventListener('click', (event) => {
+          event.target.parentElement.remove()
+      })
+      li.append(" ", button)
+
+      ul.append(li)
+
+
+    })
+  })
 });
